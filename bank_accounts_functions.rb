8b6e5455a0f241728456d5a142bbeb7eb7d38f8c
cc2RG <rbgalloway88@gmail.com>
2015-11-18 17:20:03 +0000

    first method complete

---
+++ bank_accounts_functions.rb
@@ -0,0 +1,62 @@
+ACCOUNTS = [
+  {
+    holder_name: "Jay",
+    amount: 55,
+    type: "business"
+  },
+  {
+    holder_name: "Rick",
+    amount: 1,
+    type: "personal"
+  },
+  {
+    holder_name: "Keith",
+    amount: 500,
+    type: "business"
+  },
+  {
+    holder_name: "Valerie",
+    amount: 100,
+    type: "personal"
+  },
+  {
+    holder_name: "Michael",
+    amount: 5,
+    type: "personal"
+  },
+  {
+    holder_name: "Kate",
+    amount: 2000,
+    type: "business"
+  },
+  {
+    holder_name: "Tony",
+    amount: 150,
+    type: "personal"
+  },
+  {
+    holder_name: "Sandy",
+    amount: 4500,
+    type: "business"
+  },
+]
+
+def number_of_bank_accounts()
+  ACCOUNTS.length
+end
+
+def first_bank_account_holder()
+   first_account = ACCOUNTS[0]
+   first_holder = first_account[:holder_name]
+   return first_holder
+end
+
+def print_owner_names()
+
+    for ACCOUNTS.length
+    puts "#{ACCOUNTS[:holder_name]}"  
+    end  
+end  
+
+
+
